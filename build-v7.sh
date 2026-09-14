da9c8f8d800ba1ec1f99c8b509918b482d8a0550
vwbugowner1976 <vwbugowner1976@gmail.com>
2026-09-15 08:07:55 +0900

    v7: add local build and Windows copy script

---
+++ build-v7.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -euo pipefail
+
+cd "$(dirname "$0")"
+
+ai-build cargo make uf2-v7 --release
+
+OUT=/mnt/d/rmk-firmware
+mkdir -p "$OUT"
+
+cp -v rmk-pg1kb-proto-ph3-central-v7.uf2 "$OUT/"
+cp -v rmk-pg1kb-proto-ph3-peripheral-v7.uf2 "$OUT/"
+
+echo
+echo "=== SHA256 ==="
+sha256sum rmk-pg1kb-proto-ph3-central-v7.uf2
+sha256sum "$OUT/rmk-pg1kb-proto-ph3-central-v7.uf2"
+sha256sum rmk-pg1kb-proto-ph3-peripheral-v7.uf2
+sha256sum "$OUT/rmk-pg1kb-proto-ph3-peripheral-v7.uf2"
+
+echo
+echo 'DONE:'
+echo '  D:\rmk-firmware\rmk-pg1kb-proto-ph3-central-v7.uf2'
+echo '  D:\rmk-firmware\rmk-pg1kb-proto-ph3-peripheral-v7.uf2'
