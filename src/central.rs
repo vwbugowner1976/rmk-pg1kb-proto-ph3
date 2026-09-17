d46d2658667e80d7a0e7f798e4e638bdbae8ab71
vwbugowner1976 <vwbugowner1976@gmail.com>
2026-09-17 19:54:00 +0900

    v7: call Rynk custom hook through public host re-export

--- src/central.rs
+++ src/central.rs
@@ -17,7 +17,9 @@ mod keyboard_central {
         use rmk::driver::bitbang_spi::BitBangSpiBus;
 
         // Register the PG1KB private Rynk commands before the host session starts.
-        rmk::host::rynk::register_custom_handler(crate::runtime::handle_rynk_trackball);
+        // The build patch re-exports this hook from rmk::host so we do not depend
+        // on the visibility of the internal rynk module itself.
+        rmk::host::register_custom_handler(crate::runtime::handle_rynk_trackball);
 
         // PG1KB Proto PH3 right trackball wiring:
         // SCLK=P1.05, SDIO=P1.07, CS=P1.03, MOTION=P1.15 (active low).
