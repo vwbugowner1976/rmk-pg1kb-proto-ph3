4bf315ad7990f56baedfca5b6ac0ec9cdea6c25c
vwbugowner1976 <vwbugowner1976@gmail.com>
2026-09-19 08:44:53 +0900

    v8: enable process_inner action diagnostics

--- build-v7.sh
+++ build-v7.sh
@@ -26,6 +26,7 @@ bash scripts/patch_rmk_fast_host_adv.sh 50
 bash scripts/patch_rmk_split_trackball_latency.sh 0
 bash scripts/patch_rmk_split_key_diag.sh
 bash scripts/patch_rmk_keyboard_event_diag.sh
+bash scripts/patch_rmk_process_inner_action_diag.sh
 bash scripts/patch_rmk_key_action_diag.sh
 bash scripts/patch_rmk_rynk_custom_hook.sh
 bash scripts/patch_rmk_pg1kb_runtime.sh
