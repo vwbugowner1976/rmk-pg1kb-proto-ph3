c4417b6895096a18d17be5e32c62dd30fe4824d6
vwbugowner1976 <vwbugowner1976@gmail.com>
2026-09-19 08:24:39 +0900

    v8: enable resolved key-action diagnostics

--- build-v7.sh
+++ build-v7.sh
@@ -25,6 +25,7 @@ echo "  $LOCAL_RMK"
 bash scripts/patch_rmk_fast_host_adv.sh 50
 bash scripts/patch_rmk_split_trackball_latency.sh 0
 bash scripts/patch_rmk_split_key_diag.sh
+bash scripts/patch_rmk_key_action_diag.sh
 bash scripts/patch_rmk_rynk_custom_hook.sh
 bash scripts/patch_rmk_pg1kb_runtime.sh
 
