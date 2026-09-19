186a6adcee3f0dfde062bd25fd356a1735f9a005
vwbugowner1976 <vwbugowner1976@gmail.com>
2026-09-19 09:31:41 +0900

    v8: enable PG1KB BLE profile key patch

--- build-v7.sh
+++ build-v7.sh
@@ -25,6 +25,7 @@ echo "  $LOCAL_RMK"
 bash scripts/patch_rmk_fast_host_adv.sh 50
 bash scripts/patch_rmk_split_trackball_latency.sh 0
 bash scripts/patch_rmk_rynk_custom_hook.sh
+bash scripts/patch_rmk_pg1kb_ble_keys.sh
 bash scripts/patch_rmk_pg1kb_runtime.sh
 
 # Cargo.toml points at .rmk-patched/rmk-0.9.0, so this is guaranteed to build
