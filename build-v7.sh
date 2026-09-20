4befadcc32884adf98d4008535ee0243ae647601
vwbugowner1976 <vwbugowner1976@gmail.com>
2026-09-21 07:49:43 +0900

    v8 raw: apply local RMK custom message patch in build

--- build-v7.sh
+++ build-v7.sh
@@ -44,6 +44,7 @@ echo "  $LOCAL_RMK"
 
 bash scripts/patch_rmk_fast_host_adv.sh 50
 bash scripts/patch_rmk_split_trackball_latency.sh 0
+bash scripts/patch_rmk_pg1kb_custom_message.sh
 bash scripts/patch_rmk_rynk_custom_hook.sh
 bash scripts/patch_rmk_pg1kb_ble_keys.sh
 bash scripts/patch_rmk_pg1kb_runtime.sh
